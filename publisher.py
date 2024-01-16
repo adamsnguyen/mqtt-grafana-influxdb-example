b020173a246e59c7132cb5854e3308e6506e4b8a
adam.nguyen <adam.nguyen@kiniticsautomation.com>
2024-01-16 15:59:59 -0800

    updates

--- publisher.py
+++ publisher.py
@@ -2,6 +2,13 @@ import paho.mqtt.client as mqtt
 import math
 import random
 import time
+import os
+from dotenv import dotenv_values
+
+config = dotenv_values(".env")
+mqtt_host = config["MQTT_HOST"]
+mqtt_port = config["MQTT_PORT"]
+print (f"host: {mqtt_host}, port: {mqtt_port}")
 
 def generate_sine_wave_with_noise():
     i = 0
@@ -13,9 +20,10 @@ def generate_sine_wave_with_noise():
 client = mqtt.Client("P1")
 time.sleep(10)  # Wait for services to fully load
 try:
-    client.connect("mosquitto", 1883, 60)
+    client.connect(str(mqtt_host), int(mqtt_port), 60)
 except Exception as e:
-        print(f"exception: {e}")
+    print("exception on mqtt connect")
+    print(f"exception: {e}")
     
 
 generator = generate_sine_wave_with_noise()
